47db2a17dc22eca0080f04bb5e8ebe38aa5b4146
Kristof Vandam <kristof.vandam@gmail.com>
2024-01-06 10:20:55 +0000

    init

---
+++ builder.sh
@@ -0,0 +1,29 @@
+source $stdenv/setup
+PATH=$dpkg/bin:$PATH
+
+dpkg -x $src unpacked
+
+cp -r unpacked/* $out/
+rm -rf unpacked
+
+# Patch the ELF files to use the Nix dynamic linker and set the rpath to the embedded lib directory
+for bin in $out/cinc-workstation/embedded/bin/*; do
+    # If the bin is not a ELF file, skip it
+    if ! file $bin | grep -q ELF; then continue; fi
+    patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        --set-rpath "$rpath:$out/cinc-workstation/embedded/lib" \
+        $bin
+done
+
+# Create a wrapper so we can run the binaries without entering a shell
+cat > $out/cinc-workstation/bin/cw-wrapper <<EOF
+#!$SHELL
+/opt/cinc-workstation/bin/\$@
+EOF
+chmod +x $out/cinc-workstation/bin/cw-wrapper
+
+# Create a VBox network configuration allowing all IP's
+mkdir -p $out/etc/vbox
+cat > $out/etc/vbox/networks.conf <<EOF
+* 0.0.0.0/0
+EOF
---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    cinc-workstation.url = "http://downloads.cinc.sh/files/stable/cinc-workstation/23.4.1032/debian/11/cinc-workstation_23.4.1032-1_amd64.deb";
+    cinc-workstation.flake = false;
+  };
+
+  outputs = { flake-parts, ... } @ inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+  debug = true;
+    systems = [ "x86_64-linux" ];
+    perSystem = { systen, config, self', inputs', pkgs, lib, ... }: {
+      packages = rec {
+        cinc-workstation-sources = pkgs.stdenv.mkDerivation {
+          name = "cinc-workstation";
+          src = inputs.cinc-workstation;
+          dpkg = pkgs.dpkg;
+          rpath = lib.makeLibraryPath [ pkgs.libxcrypt-legacy ];
+          builder = ./builder.sh;
+        };
+        cinc-workstation-run = pkgs.buildFHSUserEnv {
+          name = "cinc-workstation-run";
+          targetPkgs = pkgs: with pkgs; [ coreutils glibc cinc-workstation-sources ];
+          extraBuildCommands = ''
+            mkdir -p $out/opt
+            ln -s ${cinc-workstation-sources}/cinc-workstation $out/opt/cinc-workstation
+          '';
+          runScript = "$out/opt/cinc-workstation/bin/cw-wrapper";
+        };
+        cinc-workstation = pkgs.stdenv.mkDerivation {
+          name = "cinc";
+          src = cinc-workstation-sources;
+          buildInputs = [ cinc-workstation-run ];
+          installPhase = ''
+            mkdir -p $out/bin
+            for bin in $src/cinc-workstation/bin/*; do
+              echo -e "#!/usr/bin/env bash\n$buildInputs/bin/cinc-workstation-run $(basename $bin) \$@" > $out/bin/$(basename $bin)
+              chmod +x $out/bin/$(basename $bin)
+            done
+          '';
+        };
+      };
+    };
+  };
+}
